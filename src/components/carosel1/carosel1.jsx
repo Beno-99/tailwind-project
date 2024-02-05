39ff1a707fd30654a1376a7580514af590a6a9be
benon <beniamin99king@gmail.com>
2024-02-05 14:40:43 +0300

    tailwind_project

--- src/components/carosel1/carosel1.jsx
+++ src/components/carosel1/carosel1.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion,useScroll } from "framer-motion";
 import NavList from "../nav-list/nav-list";
 
 import image1 from "../../assets/image/w1.jpg";
